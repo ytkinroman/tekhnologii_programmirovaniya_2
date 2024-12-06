d1ac01f7ea1c4c7efdcbaa9fd04e52b9a2723d78
ytkinroman <mc3heisenberg@gmail.com>
2024-12-06 10:43:52 +0500

    Update mp_task_02.py

--- mp_task_02.py
+++ mp_task_02.py
@@ -1,12 +1,11 @@
-import threading
-import time
-import multiprocessing
-import math
+from threading import Thread
+from time import perf_counter
+from multiprocessing import Process
+from math import sin, pi
 
-# Функции для АТ-04
 
-# запускать с n = 699998
-def fibonacci(n):  # содержимое функции не менять
+# Запускать с n=699998.
+def fibonacci(n: int) -> int:
     """Возвращает последнюю цифру n-е числа Фибоначчи."""
     if n <= 0:
         return 0
@@ -16,61 +15,76 @@ def fibonacci(n):  # содержимое функции не менять
     a, b = 0, 1
     for _ in range(2, n + 1):
         a, b = b, a + b
-    print(f'fibonacci = {b % 10}')
+
+    print(f"fibonacci = {b % 10}")
 
 
-# запускать с f, a, b, n равными соответственно math.sin, 0, math.pi, 20000000
-def trapezoidal_rule(f, a, b, n):  # содержимое функции не менять
+# Запускать с f=math.sin, a=0, b=math.pi, n=20000000.
+def trapezoidal_rule(f, a, b, n):
     """Вычисляет определенный интеграл функции f от a до b методом трапеций с n шагами."""
     h = (b - a) / n
     integral = (f(a) + f(b)) / 2.0
+
     for i in range(1, n):
         integral += f(a + i * h)
-    print(f'trapezoidal_rule = {integral * h}')
+
+    print(f"trapezoidal_rule = {integral * h}")
 
 
 def sequence():
-    # время старта start_time
-    # вычисление fibonacci от значения 699998
-    # вычисление trapezoidal_rule со значениями math.sin, 0, math.pi, 20000000
-    # время окончания end_time
-    print(f'sequence time: {end_time - start_time: 0.2f} \n')
+    start_time = perf_counter()
+
+    fibonacci(699998)
+    trapezoidal_rule(sin, 0, pi, 20000000)
+
+    end_time = perf_counter()
+
+    print(f"sequence time: {end_time - start_time: 0.2f} \n")
 
 
 def threads():
-    # время старта start_time
-    # вычисления на потоках:
-    # 1. вычисление fibonacci от значения 699998
-    # 2. вычисление trapezoidal_rule со значениями math.sin, 0, math.pi, 20000000
-    # время окончания end_time
-    print(f'threads time: {end_time - start_time: 0.2f} \n')
+    start_time = perf_counter()
+
+    thread_fib = Thread(target=fibonacci, args=(699998,))
+    thread_trap = Thread(target=trapezoidal_rule, args=(sin, 0, pi, 20000000))
+
+    thread_fib.start()  # Запуск потока.
+    thread_trap.start()
+
+    thread_fib.join()  # Ожидание завершения потока.
+    thread_trap.join()
+
+    end_time = perf_counter()
+
+    print(f"threads time: {end_time - start_time: 0.2f} \n")
 
 
 def processes():
-    # время старта start_time
-    # вычисления на процессах:
-    # 1. вычисление fibonacci от значения 699998
-    # 2. вычисление trapezoidal_rule со значениями math.sin, 0, math.pi, 20000000
-    # время окончания end_time
-    print(f'processes time: {end_time - start_time: 0.2f} \n')
+    start_time = perf_counter()
+
+    process_fib = Process(target=fibonacci, args=(699998,))
+    process_trap = Process(target=trapezoidal_rule, args=(sin, 0, pi, 20000000))
+
+    process_fib.start()
+    process_trap.start()
+
+    process_fib.join()
+    process_trap.join()
+
+    end_time = perf_counter()
+
+    print(f"processes time: {end_time - start_time: 0.2f} \n")
 
 
-if __name__ == '__main__':
-    sequence()
-    threads()
+def main():
+    print("=" * 25)
+
+    # sequence()
+    # threads()
     processes()
-    """
-        Результатом должно стать (знаки вопроса заменятся на ваше время выполнения):
-        
-        fibonacci = 9
-        trapezoidal_rule = 2.000000000000087
-        sequence time:  ???
-        
-        fibonacci = 9
-        trapezoidal_rule = 2.000000000000087
-        threads time:  ??? 
-        
-        fibonacci = 9
-        trapezoidal_rule = 2.000000000000087
-        processes time:  ???
-    """
+
+    print("=" * 25)
+
+
+if __name__ == "__main__":
+    main()
